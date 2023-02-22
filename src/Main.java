978cb5440e4bb084176f800eace5a3c84d42d921
alinaserinia6 <naserinia6>
2023-02-22 17:26:52 +0330

    init commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner q = new Scanner(System.in);
+        int a = q.nextInt();
+        String c = q.next();
+        int b = q.nextInt();
+        switch (c) {
+            case "+":
+                System.out.println("output: " + (a + b) + "\n");
+                break;
+            case "-":
+
+        }
+    }
+}
